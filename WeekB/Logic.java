25b48ff712ed36a9e498f79178aa2bd8f26f9cd5
github_Hekuta <rotcehlugo@gmail.com>
2019-06-21 11:33:49 -0400

    Logic for (){}thing

---
+++ WeekB/Logic.java
@@ -0,0 +1,31 @@
+
+public class Logic {
+	
+	
+	
+	
+
+	public static boolean oppRecursion(String string) {
+		char opp = 0;
+		
+		if (string.length() == 0 || string.length() == 1) {
+			return false;
+		}
+		
+		if(string.charAt(0)=='[') {
+			opp=']';
+		}
+		else if(string.charAt(0)=='{') {
+			opp='{';
+		}
+		if(string.charAt(0)=='(') {
+			opp=')';
+		}
+		
+		
+		boolean a = opp == string.charAt(string.length() - 1);
+
+		return a || oppRecursion(string.substring(1, string.length() - 1));
+
+	}
+}
