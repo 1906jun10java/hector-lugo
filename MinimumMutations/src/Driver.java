b83ae2820205792895c4cb455b5628d38ba8adb7
github_Hekuta <rotcehlugo@gmail.com>
2019-06-24 13:44:15 -0400

    idk

--- MinimumMutations/src/Driver.java
+++ MinimumMutations/src/Driver.java
@@ -1,15 +1,51 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
+		
 		String start="AACCGGTT";
-		String end="AACCGGTA";
+		String end="AACAGGTA";
 		
 		//AAACGGTA
-		String[] bank= {"AACCGGTA","AACCGCTA","AACCGGTA"};
+		String[] bank= {"AACCGGTA","AACAGGTA","AACCGGTA","AACAGGTA"};
+		
+		
+		
+		//File file =new File("newFile.txt");
+		//System.out.println(file.canRead());
+		
+		//Scanner scan = new Scanner((file));		
+		
+		//System.out.println("Input the starting pattern");
+		//String start=scan.next();
+		//System.out.println("Input the ending pattern");
+		//String end=scan.next();
+		//System.out.println("Input bank length");
+		/*
+		while(!scan.hasNextInt()) {
+			System.out.println("Invalid input");
+			scan.next();
+		}
+		int len=scan.nextInt();
+		String[] bank= new String[len];
+		
+		for (int i=0;i<len;i++) {
+			System.out.println("Input bank pattern i="+i);
+			bank[i]=scan.next();
+		}
+		
+		scan.close();
+		System.out.println(Arrays.toString(bank));
 		
 		System.out.println(Utility.validCheck(start, end, bank));
+		*/
+		//scan.close();
+		//System.out.println(start);
 		
+		System.out.println(Utility.validCheck(start, end, bank));
 	}
 
 }
--- MinimumMutations/src/Utility.java
+++ MinimumMutations/src/Utility.java
@@ -1,55 +1,53 @@
 
 public class Utility {
-	
-	public static int validCheck(String s,String e,String[] b) {
-		
-		if(s==e) {
+
+	public static int validCheck(String s, String e, String[] b) {
+
+		if (s == e) {
 			return 0;
 		}
-		
-		if(e!=b[b.length-1]) {
+
+		if (e != b[b.length - 1]) {
 			return -1;
 		}
-		
-		for (int i=0;i<b.length;i++) {
-			int counter=0;
-			b[i]=b[i].replaceAll("([^ACGT])", "");
-			if(s.length()!=b[i].length()) {
+
+		for (int i = 0; i < b.length; i++) {
+			int counter = 0;
+			b[i] = b[i].replaceAll("([^ACGT])", "");
+			if (s.length() != b[i].length()) {
 				return -1;
 			}
-			
-			if (s.length()!=e.length()) {
+
+			if (s.length() != e.length()) {
 				return -1;
 			}
-			
-			for(int j=0;j<s.length();j++) {
-				if(i==0) {
-					
-					
-					if(s.charAt(j)!=b[i].charAt(j)) {
+
+			for (int j = 0; j < s.length(); j++) {
+				if (i == 0) {
+
+					if (s.charAt(j) != b[i].charAt(j)) {
 						counter++;
-						
+
 					}
-				}
-				else if(i>0) {
-					
-					if(b[i-1].charAt(j)!=b[i].charAt(j)) {
+				} else if (i > 0) {
+
+					if (b[i - 1].charAt(j) != b[i].charAt(j)) {
 						counter++;
-						
+
 					}
 				}
 			}
-			if (counter>1) {
+			if (counter > 1||counter<1) {
 				return -1;
 			}
 		}
-		
-		return b.length-1;
-	
-	}
-	
-	
-	
-	
+		int counter = 0;
+		for (int j = 0; j < s.length(); j++) {
+			if (s.charAt(j) != e.charAt(j)) {
+				counter++;
+			}
 
+		}
+		return counter;
+	}
 }
