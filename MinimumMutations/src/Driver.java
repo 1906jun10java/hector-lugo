86c375ca16b58a0ab309072fbf7c10cfa89765dd
github_Hekuta <rotcehlugo@gmail.com>
2019-06-24 13:45:01 -0400

    a

--- MinimumMutations/src/Driver.java
+++ MinimumMutations/src/Driver.java
@@ -14,36 +14,6 @@ public class Driver {
 		
 		
 		
-		//File file =new File("newFile.txt");
-		//System.out.println(file.canRead());
-		
-		//Scanner scan = new Scanner((file));		
-		
-		//System.out.println("Input the starting pattern");
-		//String start=scan.next();
-		//System.out.println("Input the ending pattern");
-		//String end=scan.next();
-		//System.out.println("Input bank length");
-		/*
-		while(!scan.hasNextInt()) {
-			System.out.println("Invalid input");
-			scan.next();
-		}
-		int len=scan.nextInt();
-		String[] bank= new String[len];
-		
-		for (int i=0;i<len;i++) {
-			System.out.println("Input bank pattern i="+i);
-			bank[i]=scan.next();
-		}
-		
-		scan.close();
-		System.out.println(Arrays.toString(bank));
-		
-		System.out.println(Utility.validCheck(start, end, bank));
-		*/
-		//scan.close();
-		//System.out.println(start);
 		
 		System.out.println(Utility.validCheck(start, end, bank));
 	}
